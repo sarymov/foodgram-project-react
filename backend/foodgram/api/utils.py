9b48d058791f1fac805cf1384d25727cba98d43e
sarymov <113104479+sarymov@users.noreply.github.com>
2023-05-22 17:25:18 +0300

    3

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -21,10 +21,12 @@ class Base64ImageField(serializers.ImageField):
 def create_ingredients(ingredients, recipe):
     ingredient_list = []
     for ingredient in ingredients:
+        current_ingredient = ingredient
         amount = ingredient.get('amount')
         ingredient_list.append(
             RecipeIngredient(
                 recipe=recipe,
+                ingredient=current_ingredient,
                 amount=amount
             )
         )
